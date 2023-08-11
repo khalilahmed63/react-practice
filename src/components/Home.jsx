f49ffef140a094a2cd63ad5a2f0a74e6c30799e0
khalilahmed63 <panhwarkhalilahmed@gmail.com>
2023-08-11 22:22:32 +0500

    useState added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,16 +6,15 @@ export default function Home() {
         <div>
             <h1 className="text-3xl font-bold underline">
                 Hello world!
-                <NewCard id={'123223'} autherName='saad'>
-                </NewCard>
-                <NewCard id={'123223'} autherName='jhatooo'>
-                </NewCard>
-                <NewCard id={'123223'} autherName='harami jhatooo '>
-                </NewCard>
-                <NewCard id={'123223'}>
-                    <p className="text-red-400">this is a pergraph</p>
-                </NewCard>
             </h1>
+            <div className="flex justify-between max-w-screen-2xl">
+                <NewCard id={'123223'} autherName='saad' />
+                <NewCard id={'123223'} autherName='jhatooo' />
+                <NewCard id={'123223'} autherName='harami jhatooo' />
+                <NewCard id={'123223'}>
+                    <p className="text-red-400">this is Children prop</p>
+                </NewCard>
+            </div>
         </div>
     )
 }
